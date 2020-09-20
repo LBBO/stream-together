d891ad124ea2bc18f059ce0dbd0e4021009c5693
Michael Kuckuk <mdk@kuckuk.com>
2020-09-20 16:07:35 +0200

    Add functional video controls for Netflix

--- src/plugin/contentScript/playerAdaption.ts
+++ src/plugin/contentScript/playerAdaption.ts
@@ -22,11 +22,68 @@ const getDefaultControls = (video: HTMLVideoElement): VideoControls => (
   }
 )
 
+const togglePlayingViaButton = (playPauseButton: HTMLElement | null) => {
+  if (playPauseButton) {
+    playPauseButton.click()
+  } else {
+    throw new Error(
+      `Play / Pause button could not be found`)
+  }
+}
+
 const getControlsForNetflix = (video: HTMLVideoElement): VideoControls => {
+  const seekBarSelector = '#appMountPoint .track'
+  const playPauseButtonSelector = '#appMountPoint .button-nfplayerPlay, #appMountPoint .button-nfplayerPause'
+
   return {
     ...getDefaultControls(video),
-    seek: () => {
-      throw new Error('Seek is not yet supported for Netflix.')
+    play: () => {
+      if (video.paused) {
+        const playPauseButton = document.querySelector<HTMLButtonElement>(playPauseButtonSelector)
+        console.log('playing')
+        togglePlayingViaButton(playPauseButton)
+      }
+    },
+    pause: () => {
+      if (!video.paused) {
+        const playPauseButton = document.querySelector<HTMLButtonElement>(playPauseButtonSelector)
+        console.log('pausing')
+        togglePlayingViaButton(playPauseButton)
+      }
+    },
+    seek: (time) => {
+      const seekBar = document.querySelector<HTMLElement>(seekBarSelector)
+
+      if (seekBar) {
+        // Seek bar might currently be hidden and could have all dimensions set to 0. Triggering a click event
+        // will show the bar and reveal its true dimensions.
+        seekBar.click()
+
+        const { width, top, left } = seekBar.getBoundingClientRect()
+
+        const percentage = time / video.duration
+        const xPosition = left + width * percentage
+
+        const fireEvent = (eventType: string) => {
+          const event = document.createEvent('MouseEvent')
+          event.initMouseEvent(
+            eventType,
+            true /* bubble */, true /* cancelable */,
+            window, NaN,
+            xPosition, top, xPosition, top, /* coordinates */
+            false, false, false, false, /* modifier keys */
+            0 /*left*/, null,
+          )
+          seekBar.dispatchEvent(event)
+        }
+
+        console.log('seeking')
+
+        fireEvent('mousedown') // Indicate this is where I plan to seek to
+        fireEvent('mouseup') // Indicate that I am done scrubbing along the bar, thus triggering the seek event
+      } else {
+        throw new Error('Seek bar not found!')
+      }
     },
   }
 }
@@ -37,27 +94,16 @@ export const getDisneyPlusPlayPauseElement = () => document.querySelector<HTMLBu
 )
 
 const getControlsForDisneyPlus = (video: HTMLVideoElement): VideoControls => {
-  const togglePlaying = () => {
-    const playPauseButton = getDisneyPlusPlayPauseElement()
-
-    if (playPauseButton) {
-      playPauseButton.click()
-    } else {
-      throw new Error(
-        `Play / Pause button could not be found and Disney Plus doesn't seem to support normal play / pause`)
-    }
-  }
-
   return {
     ...getDefaultControls(video),
     play: () => {
       if (video.paused) {
-        togglePlaying()
+        togglePlayingViaButton(getDisneyPlusPlayPauseElement())
       }
     },
     pause: () => {
       if (!video.paused) {
-        togglePlaying()
+        togglePlayingViaButton(getDisneyPlusPlayPauseElement())
       }
     },
   }
