3b9e37f8aa3c5d15fa918870ca9551a011f283f4
Michael Kuckuk <mdk@kuckuk.com>
2020-09-20 23:20:10 +0200

    Add log statement for when too many sessions are active

--- src/server/routes/createSession.ts
+++ src/server/routes/createSession.ts
@@ -11,6 +11,7 @@ export const createSession = (sessions: SessionsObject) => (req: Request, res: R
     res.status(201)
     res.send(session.data)
   } else {
+    console.log('Too many active session!', Object.values(sessions).length, Object.values(sessions))
     res.sendStatus(500)
   }
 }
