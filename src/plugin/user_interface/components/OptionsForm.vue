d5c980ea0a5266d1aa5b711b818e18b0ec00ca65
Michael Kuckuk <mdk@kuckuk.com>
2020-12-27 13:06:19 +0100

    Notify user when settings were saved

--- src/plugin/user_interface/components/OptionsForm.vue
+++ src/plugin/user_interface/components/OptionsForm.vue
@@ -14,39 +14,71 @@
       />
     </fieldset>
   </form>
+  <small :class="isSaving ? '' : 'success'">
+    {{isSaving ? 'Saving settings...' : `✔ All settings saved! (${isSavingLastChanged.toLocaleTimeString()})`}}
+  </small>
 </template>
 
 <script lang="ts">
-import { defaultOptions, getOptions, Options, saveOptions } from '../options'
+import { defaultOptions, getOptions, saveOptions } from '../options'
+import { defineComponent, onMounted, ref, watch } from 'vue'
 
-export default {
-  name: 'OptionsForm',
-  props: {
-    msg: String,
-  },
-  data(): Options {
-    return defaultOptions
-  },
-  created() {
-    getOptions()
-      .then(options => {
-        const keys = Object.keys(options) as Array<keyof Options>
+export default defineComponent({
+    name: 'OptionsForm',
+    props: {
+      msg: String,
+    },
+    setup: () => {
+      const backendURL = ref(defaultOptions.backendURL)
+      const isSaving = ref(false)
+      const isSavingLastChanged = ref(new Date())
 
-        keys.forEach(key => {
-          this[key] = options[key]
+      const setIsSaving = (newValue: boolean) => {
+        const now = new Date()
+
+        isSaving.value = newValue
+        isSavingLastChanged.value = now
+
+        return now
+      }
+
+      const setIsSavingIfHasntChangedSince = (newValue: boolean, date = new Date()) => {
+        const now = new Date()
+
+        if (date.valueOf() >= isSavingLastChanged.value.valueOf()) {
+          isSaving.value = newValue
+          isSavingLastChanged.value = now
+        }
+
+        return now
+      }
+
+      watch(backendURL, async () => {
+        const beginningOfSaving = setIsSaving(true)
+        isSaving.value = true
+        await saveOptions({
+          ...defaultOptions,
+          backendURL: backendURL.value,
         })
+        setIsSavingIfHasntChangedSince(false, beginningOfSaving)
       })
-  },
-  watch: {
-    backendURL() {
-      saveOptions(this.$data)
+
+      onMounted(async () => {
+        const options = await getOptions()
+        backendURL.value = options.backendURL
+      })
+
+      return { backendURL, isSaving, isSavingLastChanged }
     },
   },
-}
-</script>
+)</script>
 
 <style scoped lang="scss">
 fieldset {
   border-radius: 5px;
+}
+
+.success {
+  color: green;
 }
 </style>
